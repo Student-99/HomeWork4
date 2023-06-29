b7fe378a71f3179b4b96279b6570a07d16d37e9b
Student-99 <denis.0131@mail.ru>
2023-06-29 15:43:31 +0500

    Дополнил ДЗ

--- HomeWork4/src/main/java/ru/yandex/BrokenOrder.java
+++ HomeWork4/src/main/java/ru/yandex/BrokenOrder.java
@@ -1,7 +1,10 @@
 package ru.yandex;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class BrokenOrder {
@@ -20,17 +23,17 @@ public class BrokenOrder {
         return count;
     }
 
-    public static int countBrokenOrderV2(String line){
+    public static int countBrokenOrderV2(String line) {
         List<String> array = new ArrayList<>(List.of(line.split(" ")));
         List<Integer> arrayInt = new ArrayList<>();
 
-        for (String s:array){
+        for (String s : array) {
             arrayInt.add(Integer.parseInt(s));
         }
         int count = 0;
 
         for (int i = 1; i < arrayInt.size(); i++) {
-            if (arrayInt.get(i-1) > arrayInt.get(i)) {
+            if (arrayInt.get(i - 1) > arrayInt.get(i)) {
                 count++;
             }
         }
@@ -38,22 +41,64 @@ public class BrokenOrder {
 
     }
 
-    public static int countBrokenOrderV3(String line){
+    public static int countBrokenOrderV3(String line) {
         String[] arrayString = line.split(" ");
-        int[]arrayInt =new int[arrayString.length];
+        int[] arrayInt = new int[arrayString.length];
 
-        for (int i =0; i<arrayString.length;i++){
-            arrayInt[i]= Integer.parseInt(arrayString[i]);
+        for (int i = 0; i < arrayString.length; i++) {
+            arrayInt[i] = Integer.parseInt(arrayString[i]);
         }
 
         int count = 0;
 
         for (int i = 1; i < arrayInt.length; i++) {
-            if (arrayInt[i-1] > arrayInt[i]) {
+            if (arrayInt[i - 1] > arrayInt[i]) {
                 count++;
             }
         }
         return count;
     }
+
+    public static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long maxTwinMultiply(int[] array) {
+        int max1 = array[0];
+        int max2 = Integer.MIN_VALUE;
+
+        for (int i = 1; i < array.length; i++) {
+            if (max1 < array[i]) {
+                max2 = max1;
+                max1 = array[i];
+            } else if (max2 < array[i] & max1 != array[i]) {
+                max2 = array[i];
+            }
+        }
+        return max1 * max2;
+    }
+
+    public static String rleCompress(String src) {
+        Map<String ,Integer> map = new HashMap<>();
+        while (src.length()!=0){
+           char first =  src.charAt(0);
+           map.put(String.valueOf(first),src.replaceAll("[^"+first+"]","").length());
+           src = src.replaceAll(String.valueOf(src.charAt(0)),"").trim();
+        }
+        String result="";
+
+        for(Map.Entry<String ,Integer> entry: map.entrySet()){
+            result += String.format("%d(%s)",entry.getValue(),entry.getKey());
+
+        }
+
+            return result;
+    }
+
 
 }
--- HomeWork4/src/test/java/Test.java
+++ HomeWork4/src/test/java/Test.java
@@ -1,6 +1,12 @@
+
+
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import ru.yandex.BrokenOrder;
 
@@ -30,5 +36,41 @@ public class Test {
     void testV3(String line, int expected) {
         int count = BrokenOrder.countBrokenOrderV3(line);
         Assertions.assertEquals(expected, count);
+    }
+
+    @ParameterizedTest
+    @MethodSource("arraysForIsSortedTest")
+    void isSortedTest(int[] array, boolean isSorted) {
+        Assertions.assertEquals(BrokenOrder.isSorted(array), isSorted);
+    }
+
+    @ParameterizedTest
+    @MethodSource("arraysForMaxTwinMultiplyTest")
+    void maxTwinMultiplyTest(int[] array, int expected) {
+        Assertions.assertEquals(BrokenOrder.maxTwinMultiply(array), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "aaaabbbcdd,4(a)3(b)1(c)2(d)",
+        "abc,1(a)1(b)1(c)",
+        "aaaaaaaaaa,10(a)"
+    })
+    void rleCompressTest(String line,String expected){
+        Assertions.assertEquals(BrokenOrder.rleCompress(line),expected);
+    }
+
+    private static Stream<Arguments> arraysForMaxTwinMultiplyTest() {
+        return Stream.of(
+            Arguments.arguments(new int[]{9, 2, 3, 4, 5, 6, 7, 8, 9}, 72),
+            Arguments.arguments(new int[]{-9, -2, -3, -4, -5, -6, -7, -8, -9}, 6)
+        );
+    }
+
+    private static Stream<Arguments> arraysForIsSortedTest() {
+        return Stream.of(
+            Arguments.arguments(new int[]{7, 2, 3, 4, 5, 7, 6}, false),
+            Arguments.arguments(new int[]{1, 2, 3, 4, 5, 6, 7}, true)
+        );
     }
 }
